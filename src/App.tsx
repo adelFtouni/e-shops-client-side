7e52d9e428cbd2dd11c6e33bc6255c64b750cae1
adelftouni <adel.ftouni020@gmail.com>
2025-10-29 10:13:17 +0300

    asduasf

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <>
      <h1 className='text-bold underline'>hello friends</h1>
      <button className="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+     asf
     </>
   )
 }
