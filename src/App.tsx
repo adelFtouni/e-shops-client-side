86cce52e4405df37321b96060aa61d8c9f9e975b
adelftouni <adel.ftouni020@gmail.com>
2025-10-29 10:07:37 +0300

    setting up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-bold underline'>hello friends</h1>
+     <button className="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+    </>
+  )
+}
+
+export default App
