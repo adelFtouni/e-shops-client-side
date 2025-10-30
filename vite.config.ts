6436c6eb571383390c52139cf2e296981b1591f8
adelftouni <adel.ftouni020@gmail.com>
2025-10-30 06:50:23 +0300

    sync from local side

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/e-shops-client/-side/',
+  base: '/e-shops-client-side/',
   plugins: [react(),tailwindcss()
   ]     
 })
